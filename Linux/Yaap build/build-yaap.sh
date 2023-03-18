98a240b0eb6bdc05d8cac789b10c7e477c34c7e3
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-03-18 20:49:09 +0800

    linux: yaap build: add a function to sync and build dynamic partitions or non dynamic partitions YAAP

--- Linux/Yaap build/build-yaap.sh
+++ Linux/Yaap build/build-yaap.sh
@@ -5,15 +5,52 @@ cd ${PWD}
 threads=""
 CUSTOM_NAME=""
 BUILD_SIGNED=false # build unsigned by default for now
+BUILD_DYNAMIC=""
+
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+LRD='\033[1;31m'
+LGR='\033[1;32m'
+YEL='\033[1;33m'
+CYN='\033[1;36m'
+
+function print()
+{
+	echo -e ${1} "\r${2}${NC}"
+}
 
 function cleanup()
 {
   rm .android-certs # This should be a symbolic link NOT the actual folder
+
+  if [ -e ".repo/local_manifests/yaap_manifest.xml-bak" ]; then
+    print "${CYN}Restoring previous yaap_manifest.xml"
+    mv .repo/local_manifests/yaap_manifest.xml-bak .repo/local_manifests/yaap_manifest.xml
+  fi
 }
 
 function setup_signed()
 {
   ln -s ~/.android-certs .android-certs
+}
+
+function sync_build_type()
+{
+  if [ -e ".repo/local_manifests/yaap_manifest.xml" ]; then
+    mv .repo/local_manifests/yaap_manifest.xml .repo/local_manifests/yaap_manifest.xml-bak
+  fi
+
+  if [ "$BUILD_DYNAMIC" = true ]; then
+    print "${CYN}Getting dynamic partitions yaap_manifest.xml"
+    wget -q https://raw.githubusercontent.com/pwnrazr/device_xiaomi_raphael/thirteen-dynamic-partitions/yaap_manifest.xml -P .repo/local_manifests/
+  elif [ "$BUILD_DYNAMIC" = false ]; then
+    print "${CYN}Getting non dynamic partitions yaap_manifest.xml"
+    wget -q https://raw.githubusercontent.com/pwnrazr/device_xiaomi_raphael/thirteen/yaap_manifest.xml -P .repo/local_manifests/
+  fi
+
+  print "${CYN}Synchronizing device and kernel tree"
+  repo sync -q device/xiaomi/raphael/ kernel/xiaomi/sm8150/
 }
 
 while [[ $# -gt 0 ]]; do
@@ -43,6 +80,24 @@ while [[ $# -gt 0 ]]; do
       setup_signed
       shift 1
       ;;
+    --dynamic)
+      if [ -z "$BUILD_DYNAMIC" ]; then
+        BUILD_DYNAMIC=true
+      else
+        print "${RED}Both --dynamic and --non-dynamic flags set!"
+        exit
+      fi
+      shift 1
+      ;;
+    --non-dynamic)
+      if [ -z "$BUILD_DYNAMIC" ]; then
+        BUILD_DYNAMIC=false
+      else
+        print "${RED}Both --dynamic and --non-dynamic flags set!"
+        exit
+      fi
+      shift 1
+      ;;
     *)
       echo "Invalid option: $key"
       exit 1
@@ -50,20 +105,13 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+if [ "$BUILD_DYNAMIC" != "" ]; then
+  sync_build_type
+else
+  print "${YEL}Building current"
+fi
+
 #echo current dir: ${PWD}
-
-# Colors
-NC='\033[0m'
-RED='\033[0;31m'
-LRD='\033[1;31m'
-LGR='\033[1;32m'
-YEL='\033[1;33m'
-CYN='\033[1;36m'
-
-function print()
-{
-	echo -e ${1} "\r${2}${NC}"
-}
 
 export USE_CCACHE=true
 export CCACHE_DIR=/home/pwnrazr/dev-stuff/yaap-ccache
